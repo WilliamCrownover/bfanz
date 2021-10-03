537147feabdce105517356bb1c90c6ae2feb30aa
j92pruitt <j92pruitt@gmail.com>
2021-10-03 14:14:21 -0700

    Added a couple dev packages, and production options to server.

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ try {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+    }
+    
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
