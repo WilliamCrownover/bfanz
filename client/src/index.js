a631f3ed5ea8565ffcd12d79ebecf5f05ab634c0
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-09 20:00:07 -0700

    finally installed the correct dependencies for mui

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ClippedDrawer from './components/ClippedDrawer'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ClippedDrawer />
   </React.StrictMode>,
   document.getElementById('root')
 );
