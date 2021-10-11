05194ed9ca2838bd7532696aad00483d172b1fb5
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-10 22:48:02 -0700

    re-nested components

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ResponsiveDrawer from './components/ResponsiveDrawer'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <ResponsiveDrawer />
   </React.StrictMode>,
   document.getElementById('root')
 );
