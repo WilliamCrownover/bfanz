1f920b517f5fa5f15d76e69f7d6161b2935eaa35
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-12 20:47:05 -0700

    added a Container wrapper around the dashboard grid to horizontally center things more

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,19 +1,21 @@
 import Grid from '@mui/material/Grid';
 import DashCard from '../components/DashCard';
 import AddMovieButton from '../components/AddMovieButton';
+import Container from '@mui/material/Container';
 
 const Dashboard = () => {
 
     return (
-
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-            <Grid item>
-                <AddMovieButton />
+        <Container>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                <Grid item>
+                    <AddMovieButton />
+                </Grid>
+                <Grid item>
+                    <DashCard />
+                </Grid>
             </Grid>
-            <Grid item>
-                <DashCard />
-            </Grid>
-        </Grid>
+        </Container>
     );
 };
 
