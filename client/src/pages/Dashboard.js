fefa7910f548a5340b57e14f194369761926e447
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-12 21:54:28 -0700

    removed comment

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -6,7 +6,6 @@ import SearchBar from '../components/SearchBar/SearchBar';
 import Box from '@mui/material/Box';
 
 const Dashboard = () => {
-    // sx={{ display: 'flex', justifyContent: 'flex-start' }}
     return (
         <Container>
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
