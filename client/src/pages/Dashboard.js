1641da0fe6b5b3c48f5dae09215188bf6661b19c
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-12 21:46:51 -0700

    added a search bar component and put it in the dashboard page so it sits on top of them

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,12 +2,19 @@ import Grid from '@mui/material/Grid';
 import DashCard from '../components/DashCard';
 import AddMovieButton from '../components/AddMovieButton';
 import Container from '@mui/material/Container';
+import SearchBar from '../components/SearchBar/SearchBar';
+import Box from '@mui/material/Box';
 
 const Dashboard = () => {
-
+    // sx={{ display: 'flex', justifyContent: 'flex-start' }}
     return (
         <Container>
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                <Grid item xs={12}>
+                    <Box maxWidth="sm" >
+                        <SearchBar />
+                    </Box>
+                </Grid>
                 <Grid item>
                     <AddMovieButton />
                 </Grid>
