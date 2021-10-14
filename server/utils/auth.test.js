aae5353398073562497fc77bc6dff17b09028411
j92pruitt <j92pruitt@gmail.com>
2021-10-13 18:44:07 -0700

    Created tests for sign token function.

--- server/utils/auth.test.js
+++ server/utils/auth.test.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const {signToken} = require('./auth');
+const jwt = require('jsonwebtoken');
 
 describe('Sign Token Function', () => {
 
@@ -7,6 +8,32 @@ describe('Sign Token Function', () => {
         
         const input = {username: "Fred", _id: "21"};
 
-        expect(signToken(input)).toMatch(/([a-zA-z0-9]+\.)([a-zA-z0-9]+\.).+/gm)
+        expect(signToken(input)).toMatch(/(.+\.)(.+\.).+/gm);
     });
+
+    test('it should throw an error if username is not provided', () => {
+
+        const input = {_id: "21"};
+
+        expect( () => {signToken(input)} ).toThrow(Error);
+    });
+
+    test('it should throw an error if _id is not provided', () => {
+
+        const input = {username: "Fred"};
+
+        expect( () => {signToken(input)} ).toThrow(Error);
+    })
+
+    test('it should verify correctly using jwt.verify', () => {
+
+        const input = {username: "Fred", _id: "21"};
+        const expiration = '12h'
+
+        const token = signToken(input);
+
+        const { data } = jwt.verify(token, process.env.JWT_SECRET, { maxAge: expiration });
+
+        expect(data).toEqual(input);
+    })
 });
