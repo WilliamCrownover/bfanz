d551ea9dbe1bce79aa266a81aaad9aa17fe7c482
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-13 19:16:22 -0700

    changed addmovie card button to link via a react-router-dom link to prevent refreshes

--- client/src/components/AddMovieButton/index.js
+++ client/src/components/AddMovieButton/index.js
@@ -4,20 +4,23 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import AddIcon from '@mui/icons-material/Add';
 import CardActionArea from '@mui/material/CardActionArea';
+import { BrowserRouter as Link } from 'react-router-dom';
 
 export default function BasicCard() {
     return (
-        <Card sx={{ minWidth: 275 }}>
-            <CardActionArea href="/AddMovie">
-                <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
-                    <Typography variant="h5" component="div">
-                        Add New Movie
-                    </Typography>
-                </CardContent>
-                <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
-                    <AddIcon />
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <Link to="/AddMovie">
+            <Card sx={{ minWidth: 275 }}>
+                <CardActionArea>
+                    <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Typography variant="h5" component="div">
+                            Add New Movie
+                        </Typography>
+                    </CardContent>
+                    <CardContent sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <AddIcon />
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Link>
     );
 }
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,5 +1,6 @@
 import TextField from '@mui/material/TextField';
 
+
 export default function SearchBar() {
     return (
         <TextField fullWidth id="outlined-basic" label="Find Movies" variant="outlined" />
