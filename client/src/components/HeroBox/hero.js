19862563138fc4a5cf8d0f522b2d735a7dca3365
grudgecat <grudgecat@protonmail.com>
2021-10-13 19:59:06 -0700

    update hero img

---
+++ client/src/components/HeroBox/hero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import hero from '../..assets/hero.jpg'; 
+
+function Hero() {
+  // Import result is the URL of your image
+  return <img src={hero} alt="Hero" />;
+}
+
+export default Hero;
