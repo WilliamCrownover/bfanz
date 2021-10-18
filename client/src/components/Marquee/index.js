722528c8b68caa327012553e74fb1a1671e1c367
grudgecat <grudgecat@protonmail.com>
2021-10-17 21:09:22 -0700

    changed marquee css and images, unable to get z-index working properly - work in progress

--- client/src/components/Marquee/index.js
+++ client/src/components/Marquee/index.js
@@ -4,13 +4,16 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import './assets/Marquee.css';
 
-
+{/* <img src='/assets/marq1.jpg' alt="Silent Film Title Card" /> */}
 
 export default function Marquee() {
     return (
-        <Grid item>
-            <Box class="filmBackground">
-            <img src='/assets/marq1.jpg' alt="Silent Film Title Card" />
+        <Grid >
+            <Box sx={{height: 420}} class = 'cellBackground'>
+                <img src="assets/filmCell.jpg"/>
+                <Box class = 'filmBackground' >
+                <img src='/assets/titleCard.jpg' alt="Silent Film Title Card" />
+                </Box>
             </Box>
         </Grid>
     );
