576df9c46e8733f6d00d379028471b5919a7251e
William Crownover <williamcrownover1@gmail.com>
2021-10-18 20:49:26 -0700

    Created helper functions for fetching from Open Movie API and converting data for our db

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 export const percent = (num1, num2) => {
 	return parseInt((num1/(num1 + num2)) * 100);
 };
@@ -5,3 +7,32 @@ export const percent = (num1, num2) => {
 export const total = (num1, num2) => {
 	return num1 + num2;
 }
+
+export const fetchMovie = async (title) => {
+	title.replace(' ', '+');
+
+	const queryUrl = `http://www.omdbapi.com/?t=${title}&apikey=${process.env.API_KEY}`
+
+	const res = await fetch ( queryUrl );
+
+	const movieData = await res.json();
+
+	console.log(movieData);
+
+	console.log(convertMovieData(movieData));
+
+};
+
+const convertMovieData = (data) => {
+	return {
+		title: data.Title,
+		year: data.Year,
+		genre: data.Genre,
+		director: data.Director,
+		writer: data.Writer,
+		actors: data.Actors,
+		description: data.Plot,
+		poster: data.Poster,
+		indbID: data.imdbID
+	}
+};
