df7b340c0f6f4e92d9d76d8b23aeb971baf378e1
grudgecat <grudgecat@protonmail.com>
2021-10-18 21:00:08 -0700

    adjusted welcome image, removed unused image files

--- client/src/components/Marquee/index.js
+++ client/src/components/Marquee/index.js
@@ -9,8 +9,8 @@ export default function Marquee() {
     return (
         <Container>
         <Grid sx={{mb: 5}}>
-            <Box sx={{minHeight: 500, alignItems: 'center', justifyContent: 'center', padding: 3, border: '1px solid black'}} class = 'cellBackground'>
-                <img src="assets/filmCellWelcome.jpg" alt='Film Cell Border'/>
+            <Box sx={{minHeight: 500, alignItems: 'center', justifyContent: 'center', padding: 3}} class = 'cellBackground'>
+                <img src="./assets/filmCellWelcome.jpg" alt='Film Cell Border'/>
             </Box>
         </Grid>
         </Container>
