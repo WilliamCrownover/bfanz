5891577d0fec0260544c87287ea6c2180739a003
grudgecat <grudgecat@protonmail.com>
2021-10-20 16:34:47 -0700

    adj to marquee for resizing, still not working as desired

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,43 @@
-import HookQuestions from '../components/HookQuestions';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-// import HeroBox from '../components/HeroBox/index';
-import Marquee from '../components/Marquee';
-import Box from '@mui/material/Box';
-// import { makeStyles } from '@material-ui/core';
+import HookQuestions from "../components/HookQuestions";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+// import HeroBox from "../components/HeroBox/index";
+import Marquee from "../components/Marquee";
+import Box from "@mui/material/Box";
+// import { makeStyles } from "@material-ui/core";
 
 const Home = () => {
 
     return (
         <Container>
-        <Grid container sx={{ width: '95vw' }}>
-            <Grid item xs={12} sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'  }}>
+        <Grid container sx={{ width: "95vw" }}>
+            <Grid item xs={12} sx={{ width: "99vw", display: "flex", justifyContent: "center", alignItems: "center" }}>
                 <Box
-                    component='img'
-                    src='/assets/hero.jpg'
+                    component="img"
+                    src="/assets/hero.jpg"
                     sx={{
                         width: {
-                            xs: '100%',
-                            sm: '72%',
-                            md: '85%',
-                            xl: '90%'
+                            xs: "100%",
+                            sm: "72%",
+                            md: "85%",
+                            xl: "90%"
                         },
                         maxHeight: "400px",
                     }}
                 />
             </Grid>
-            <Grid item xs={12} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Marquee display={{ md: "none" }} />
+            <Grid item xs={12} sx={{width: "90vw", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Marquee  
+                    sx={{
+                        width: {
+                            xs: "100%",
+                            sm: "72%",
+                            md: "85%",
+                            xl: "90%"
+                        },
+                        maxHeight: "300px",
+                    }}
+                />
             </Grid>
             <Grid item xs={12} >
                 <HookQuestions />
