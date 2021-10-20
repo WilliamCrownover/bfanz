bc80e4ec77330d5e89322ad5de7efb73ae9c088f
grudgecat <grudgecat@protonmail.com>
2021-10-20 16:20:01 -0700

    adjustment to marquee

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,17 @@
 import HookQuestions from '../components/HookQuestions';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-import HeroBox from '../components/HeroBox/index';
+// import HeroBox from '../components/HeroBox/index';
 import Marquee from '../components/Marquee';
 import Box from '@mui/material/Box';
+// import { makeStyles } from '@material-ui/core';
 
 const Home = () => {
 
     return (
         <Container>
-        <Grid container sx={{ width: '100vw' }}>
-            <Grid item xs={12} sx={{ width: '100%' }}>
+        <Grid container sx={{ width: '95vw' }}>
+            <Grid item xs={12} sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'  }}>
                 <Box
                     component='img'
                     src='/assets/hero.jpg'
@@ -20,7 +21,8 @@ const Home = () => {
                             sm: '72%',
                             md: '85%',
                             xl: '90%'
-                        }
+                        },
+                        maxHeight: "400px",
                     }}
                 />
             </Grid>
