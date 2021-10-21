1bd26ca8be828f8962fc49692d5d437ec330ef28
j92pruitt <j92pruitt@gmail.com>
2021-10-20 19:42:46 -0700

    Cleaned up react errors and warnings.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,7 +4,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import HeroBox from '../components/HeroBox';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
