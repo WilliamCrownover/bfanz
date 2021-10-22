e843218e083c4596ac13b17cf33ed9e3af7da4de
j92pruitt <j92pruitt@gmail.com>
2021-10-21 18:29:13 -0700

    created server url request helper for omdb api.

---
+++ server/utils/helpers.js
@@ -0,0 +1,27 @@
+require('dotenv').config({path:'../.env'});
+const axios = require('axios');
+
+const convertMovieData = (data) => {
+	return {
+		title: data.Title,
+		year: data.Year,
+		genre: data.Genre,
+		director: data.Director,
+		writer: data.Writer,
+		actors: data.Actors,
+		description: data.Plot,
+		poster: data.Poster,
+		indbID: data.imdbID
+	}
+};
+
+export const fetchMovie = async (title) => {
+	title.replace(' ', '+');
+
+	const queryUrl = `http://www.omdbapi.com/?t=${title}&apikey=${process.env.OMDB_API_KEY}`
+
+	const res = await axios.get( queryUrl );
+
+	return convertMovieData(res.data);
+
+};
